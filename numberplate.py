9a8f3c708a851ee1d19e9eb36c374b36e2171636
Sivakumar Sukumaran <66319335+SivaGITHUB2020@users.noreply.github.com>
2024-10-09 13:31:54 -0400

    Add files via upload

---
+++ numberplate.py
@@ -0,0 +1,11 @@
+# Create a data base of number plates
+np = ["asd123", "qwe147", "cvb369", "sdf159"]
+
+# Enter new number plate
+newnp = input("Enter the number plate: ")
+
+# check if new number plate is in the database if not say Red
+if newnp in np:
+    print("Green, All good!")
+else:
+    print("Red, No parking permit!")
