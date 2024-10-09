746be10cf8936239c8f53088310e12d5e82f8656
Sivakumar Sukumaran <66319335+SivaGITHUB2020@users.noreply.github.com>
2024-10-09 13:36:31 -0400

    Update numberplate.py

--- numberplate.py
+++ numberplate.py
@@ -4,7 +4,7 @@ np = ["asd123", "qwe147", "cvb369", "sdf159"]
 # Enter new number plate
 newnp = input("Enter the number plate: ")
 
-# check if new number plate is in the database if not say Red
+# check if new number plate is in the database if not print Red
 if newnp in np:
     print("Green, All good!")
 else:
